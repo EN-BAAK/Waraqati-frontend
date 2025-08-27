fdd1c4f20aab9a180c15bb3efb2bf1f0f34b706a
EN-BAAK <swaiengineer@gmail.com>
2025-08-27 18:52:49 +0300

    enhance dahsboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -21,13 +21,13 @@ export const metadata: Metadata = {
 
 const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-back">
+    <div className="bg-back min-h-screen flex">
       <Sidebar />
 
       <div className="flex-1 flex flex-col">
         <Header />
 
-        <main className="flex-1 p-6 bg-back">
+        <main className="bg-back p-3 flex-1">
           {children}
         </main>
       </div>
